f19c2e6af55671e287fa4c4e97bc6e24f3436d4b
yuzumican <83267351+yuyu1815@users.noreply.github.com>
2024-05-18 15:42:28 +0900

    ノートpc

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ import os,glob
 from tqdm import tqdm
 from PIL import Image, ImageDraw
 
+
 folder_path_1 = './Not_mosaic'
 folder_path_2 = './mosaic'
 
@@ -25,7 +26,7 @@ def main():
     print(image_files_1)
     process_images(image_files_1)
     for image_file in tqdm(image_files_2):
-        white_to_transparency(f'./mosaic/{image_file}', './temp_m')
+        white_to_transparency(f'./mosaic/{image_file}', './temp')
 
 
 def white_to_transparency(img_path, output_path):
@@ -36,7 +37,7 @@ def white_to_transparency(img_path, output_path):
     newData = []
     for item in datas:
         # 白色のピクセルを透明に変更する (閾値を設定)
-        if item[0] > 220 and item[1] > 220 and item[2] > 220:
+        if item[0] == 255 and item[1] == 255 and item[2] == 255:
             newData.append((255, 255, 255, 0))
         else:
             newData.append(item)
@@ -47,11 +48,10 @@ def white_to_transparency(img_path, output_path):
 def process_images(image_files):
     reader = easyocr.Reader(language_list, gpu=True)
     for image_file in tqdm(image_files):
-        # グレースケール画像でOCRを実行
-        image_gray = Image.open(image_file).convert('L')
-        image_gray.save(f"./temp_gray/{image_file.replace('.jpg', '_gray.png')}", "PNG")
+        convert_to_black_and_white(image_file)
 
-        result = reader.readtext(f"./.temp_gray/{image_file.replace('.jpg', '_gray.png')}", paragraph=True)
+    for image_file in tqdm(image_files):
+        result = reader.readtext(f"./.temp_g/{image_file.replace('.jpg', '.png')}", paragraph=True)
         # 元のカラー画像を開く
         image = Image.open(image_file)
         draw = ImageDraw.Draw(image)
@@ -62,9 +62,28 @@ def process_images(image_files):
             polygon = [(x, y) for x, y in coordinates]
             draw.polygon(polygon, fill="white")
 
-    image.save(f"./.temp/{ image_file.replace('.jpg', '.png')}", "PNG")
-    white_to_transparency(f"./.temp/{image_file.replace('.jpg', '.png')}", "./temp_nm")
+        image.save(f"./.temp/{image_file.replace('.jpg', '.png')}", "PNG")
+
+    for image_file in tqdm(image_files):
+        white_to_transparency(f"./.temp/{image_file.replace('.jpg', '.png')}", f"./.temp/{image_file.replace('.jpg', '.png')}")
 
 
+def convert_to_black_and_white(image_path, threshold=100):
+    """
+    画像を白と黒の二値化画像に変換する関数。
+
+    :param image_path: 変換する画像のパス
+    :param threshold: 二値化の閾値（0から255の間）。この値以上を白、未満を黒とする。
+    :return: 二値化されたPillow画像オブジェクト
+    """
+    # 画像を読み込む
+    image = Image.open(image_path)
+    # グレースケールに変換
+    image = image.convert('L')
+    # 二値化を実行
+    image = image.point(lambda x: 255 if x >= threshold else 0)
+    # 二値化した画像を'L'モードで保存
+    save_path = f"./.temp_g/{image_path.replace('.jpg', '.png')}"
+    image.save(save_path, "PNG")
 
 main()
